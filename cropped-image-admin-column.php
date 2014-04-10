822567a7901ede29c8daf72f5e49ab8ba7f81584
Junior Grossi <me@juniorgrossi.com>
2014-04-09 22:02:28 -0300

    first commit

---
+++ cropped-image-admin-column.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Plugin Name: Cropped Image Admin Column
+Description: Allow users to use both "Advanced Custom Fields: Image Crop Add-on" and "Codepress Admin Columns" plugins. This plugin allow cropped images to be shown on the admin list pages.
+Author: Junior Grossi
+Version: 0.1
+Author URI: http://juniorgrossi.com
+*/
+
+class CroppedImageAdminColumn
+{
+    private $filterName = '';
+
+    public function __construct()
+    {
+        add_filter('cac/column/meta/value', array($this, 'changeValue'), 10, 3);
+    }
+
+    public function changeValue($value, $id = 0, $object = null)
+    {
+        $meta = $object->get_meta_by_id($id);
+        $meta = json_decode($meta);
+
+        if (isset($meta->cropped_image)) {
+            $meta = "{$meta->cropped_image}";
+            return $object->get_value_by_meta($meta, $id);
+        }
+
+        return $value;
+    }
+
+}
+
+new CroppedImageAdminColumn();
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Cropped Image Admin Column ===
+Contributors: jgrossi
+Tags: plugins, wordpress, admin, column, columns, custom columns, custom fields, cropped, crop image, image, adc, advanced custom fields
+Tested up to: 3.8.1
+
+Allow users to use "Image" column type with the plugin "acf-image-crop-add-on". You will see as content in the column the cropped image you've created.
+
+== Description ==
+
+Allow users to use "Image" column type with the plugin "acf-image-crop-add-on". You will see as content in the column the cropped image you've created.
+
+== Installation ==
+
+1. Upload the file to plugins folder
+2. Activate the plugin in the Wordpress admin panel
+3. Configure the Codepress Admin Columns plugin to show Image field type with a cropped image.
+4. See the result
+
+== Frequently Asked Questions ==
+
